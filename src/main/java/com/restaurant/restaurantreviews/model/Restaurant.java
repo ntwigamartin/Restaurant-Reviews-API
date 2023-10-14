25f10a476b06640f87c2965cd59206801414000a
ntwigamartin <ntwiga.martin@gmail.com>
2023-10-14 17:48:51 +0300

    tables relationships setup

--- src/main/java/com/restaurant/restaurantreviews/model/Restaurant.java
+++ src/main/java/com/restaurant/restaurantreviews/model/Restaurant.java
@@ -1,10 +1,14 @@
 package com.restaurant.restaurantreviews.model;
 
+
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -22,4 +26,7 @@ public class Restaurant {
 
     @Column(name = "location", nullable = false)
     private String location;
+
+    @OneToMany(mappedBy = "restaurant")
+    private List<Review> reviews;
 }
--- src/main/java/com/restaurant/restaurantreviews/model/Review.java
+++ src/main/java/com/restaurant/restaurantreviews/model/Review.java
@@ -5,13 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "reviews")
-public class Reviews {
+public class Review {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +21,12 @@ public class Reviews {
 
     @Column(name = "review")
     private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
